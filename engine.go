9e237af9304599a16f64d4c0e789c22978ca9b83
planetbridging <47130739+planetbridging@users.noreply.github.com>
2024-05-14 07:23:04 +1000

    added accepting all routes for cors

--- engine.go
+++ engine.go
@@ -36,7 +36,11 @@ func main() {
 
 	app := fiber.New()
 
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	setupRoutes(app)
 
